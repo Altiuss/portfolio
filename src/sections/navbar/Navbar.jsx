238889048e430bbb8c33dc7fc1661531de60a9c3
Altiuss <altiuss@inbox.lv>
2025-01-25 22:46:43 +0100

    Navigationsleiste erstellt

--- src/sections/navbar/Navbar.jsx
+++ src/sections/navbar/Navbar.jsx
@@ -1,8 +1,22 @@
+import Logo from '../../assets/logo.jpg';
+import data from './data';
+import { IoColorPalette } from "react-icons/io5";
 import './navbar.css';
+
 
 const Navbar = () => {
   return (
-    <nav><h2>Navbar</h2></nav>
+    <nav>
+      <div className="container nav__container" >
+        <a href="indeh.html" className='nav__logo'>
+          <img src={Logo} alt="Logo" />
+        </a>
+        <ul className='nav__menu'>
+          {data.map(item => <li key={item.id}><a href={item.link}>{item.title}</a></li>)}
+        </ul>
+        <button id='theme__icon'><IoColorPalette /></button>
+      </div>
+    </nav>
   )
 }
 
