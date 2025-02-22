5df7a1533805298217081c53c4dfe9525d7632ba
Altiuss <altiuss@inbox.lv>
2025-02-22 20:01:11 +0100

    added Footer

--- src/sections/footer/Footer.jsx
+++ src/sections/footer/Footer.jsx
@@ -1,8 +1,27 @@
+import { links, socials } from './data'
+
 import './footer.css';
 
 const Footer = () => {
   return (
-   <footer><h2>Footer</h2></footer>
+    <footer>
+      <div className="container footer__container">
+        <ul className='nav__menu'>
+          {
+            links.map(flink => <li key={flink.id}><a href={flink.link}>{flink.title}</a></li>)
+          }
+        </ul>
+        <div className="footer__socials">
+          {
+            socials.map(social => <a key={social.id} href={social.link} target='_blank' 
+            rel='noopener noreferrer'>{social.icon}</a> )
+          }
+        </div>
+      </div>
+      <div className="footer__copyright">
+        <small>2025 AIGARS TUTORIALS &copy; All Rights Reserved</small>
+      </div>
+    </footer>
   )
 }
 
